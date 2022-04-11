dd3aba8bb510e204fc8b2116cc2aae3f07110dec
TOXIC ALIEN <albinthomas81.nky@icloud.com>
2022-04-12 00:45:11 +0530

    ＡＬＩＥＮ ＡＬＦＡ－ＭＤ

---
+++ config.js
@@ -0,0 +1,47 @@
+//═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                                                                     //
+//                                                             ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ                                                               //
+//                                                                                                                                                     //
+//                                                                    Ｖ：１．０．１                                                                     //
+//                                                                                                                                                     //
+//                                     ░█████╗░██╗░░░░░██╗███████╗███╗░░██╗  ░█████╗░██╗░░░░░███████╗░█████╗░                                          //
+//                                     ██╔══██╗██║░░░░░██║██╔════╝████╗░██║  ██╔══██╗██║░░░░░██╔════╝██╔══██╗                                          //
+//                                     ███████║██║░░░░░██║█████╗░░██╔██╗██║  ███████║██║░░░░░█████╗░░███████║                                          //
+//                                     ██╔══██║██║░░░░░██║██╔══╝░░██║╚████║  ██╔══██║██║░░░░░██╔══╝░░██╔══██║                                          //
+//                                     ██║░░██║███████╗██║███████╗██║░╚███║  ██║░░██║███████╗██║░░░░░██║░░██║                                          //
+//                                     ╚═╝░░╚═╝╚══════╝╚═╝╚══════╝╚═╝░░╚══╝  ╚═╝░░╚═╝╚══════╝╚═╝░░░░░╚═╝░░╚═╝                                          //
+//                                                                                                                                                     //
+//                                                                                                                                                     //
+//═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════//
+
+// Obfuscated By AlienAlfa
+// Unecrypted file Willbe relesed Soon
+// Full file will be relised After all BUGS Has been Fixed
+
+// CURRENTLY RUNNING ON BETA VERSION!!
+/**
+   * MIT License
+* 
+   * Copyright (c) 2021 Alien-Alfa.
+* 
+   * Permission is hereby granted, free of charge, to any person obtaining a copy
+   * of this software and associated documentation files (the "Software"), to deal
+   * in the Software without restriction, including without limitation the rights
+   * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   * copies of the Software, and to permit persons to whom the Software is
+   * furnished to do so, subject to the following conditions:
+* 
+   * The above copyright notice and this permission notice shall be included in all
+   * copies or substantial portions of the Software.
+* 
+   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   * SOFTWARE.
+*/
+
+
+const _0xe15980=_0x10dd;(function(_0x44eaeb,_0x46a99f){const _0x2df46c=_0x10dd,_0x41aae7=_0x44eaeb();while(!![]){try{const _0x66eb09=-parseInt(_0x2df46c(0xdf))/0x1*(-parseInt(_0x2df46c(0xfa))/0x2)+-parseInt(_0x2df46c(0xec))/0x3+-parseInt(_0x2df46c(0xe0))/0x4*(-parseInt(_0x2df46c(0xd5))/0x5)+-parseInt(_0x2df46c(0x101))/0x6+parseInt(_0x2df46c(0xfd))/0x7+-parseInt(_0x2df46c(0xf1))/0x8*(-parseInt(_0x2df46c(0xd9))/0x9)+parseInt(_0x2df46c(0xd2))/0xa*(-parseInt(_0x2df46c(0xcf))/0xb);if(_0x66eb09===_0x46a99f)break;else _0x41aae7['push'](_0x41aae7['shift']());}catch(_0x3c9980){_0x41aae7['push'](_0x41aae7['shift']());}}}(_0x503a,0x6c601));const fs=require('fs'),chalk=require(_0xe15980(0xef)),apikeyset=process['env'][_0xe15980(0xf3)]===undefined?!![]:process[_0xe15980(0xe2)][_0xe15980(0xf3)];function _0x503a(){const _0x596a26=['ʏᴏᴜʀ\x20ᴅᴀɪʟʏ\x20ʟɪᴍɪᴛ\x20ʜᴀꜱ\x20ᴇxᴘɪʀᴇᴅ,\x20ᴛʜᴇ\x20ʟɪᴍɪᴛ\x20ᴡɪʟʟ\x20ʙᴇ\x20ʀᴇꜱᴇᴛ\x20ᴇᴠᴇʀʏ\x2012\x20ʜᴏᴜʀꜱ','514771iACEfh','214252PhnKqC','sessionName','env','ꜰᴇᴀᴛᴜʀᴇ\x20ᴏɴʟʏ\x20ꜰᴏʀ\x20ᴏᴡɴᴇʀ\x20ᴏꜰ\x20ᴛʜᴇ\x20ʙᴏᴛ','thumb','ꜰᴇᴀᴛᴜʀᴇꜱ\x20ᴜꜱᴇᴅ\x20ᴏɴʟʏ\x20ꜰᴏʀ\x20ɢʀᴏᴜᴘꜱ!','prefa','S_PACK','random','./lib/lowdb/adapters/don.jpg','unwatchFile','https://zenzapi.xyz','1416060NWbCNz','Update\x27','watchFile','chalk','packname','397160UwFOvB','mess','ZENS_API','7C93A5956E','resolve','owner','C927B48B97','419B9CD9DBE9','5ce9b2c90a','2iztKkD','don','process.env.OWNER','5476905DZXaDB','readFileSync','0C60476A8533','author','3502878ytpotK','ʙᴏᴛ\x20ᴍᴜꜱᴛ\x20ʙᴇ\x20ᴀᴅᴍɪɴᴛᴏ\x20ᴜꜱᴇ\x20ɪᴛ!','cache','ꜰᴇᴀᴛᴜʀᴇ\x20ᴏɴʟʏ\x20ꜰᴏʀ\x20ɢʀᴏᴜᴘ\x20ᴀᴅᴍɪɴ','5CE9B2C90A','4F7107108B','128546ZkMEpN','premium','limitawal','190UyOgEy','log','447405935355','35YCIXWl','session.alfa','./lib/lowdb/adapters/alfa.jpg','APIKeys','9OaFyth','redBright','ꜰᴇᴀᴛᴜʀᴇꜱ\x20ᴍᴀᴅᴇ\x20ᴏɴʟʏ\x20ꜰᴏʀ\x20ᴘʀɪᴠᴀᴛᴇ\x20ᴄʜᴀᴛ!','Infinity','1544701A60'];_0x503a=function(){return _0x596a26;};return _0x503a();}global['APIs']={'zenz':_0xe15980(0xeb)};if(apikeyset===!![]){const apik=[_0xe15980(0xf9),_0xe15980(0xcd),_0xe15980(0xdd),'10F0975E30',_0xe15980(0xf8),_0xe15980(0xf7),_0xe15980(0xf4),'F5DBD2C1CF',_0xe15980(0xff),_0xe15980(0xce)],api=apik[Math['floor'](Math[_0xe15980(0xe8)]()*apik['length'])];global[_0xe15980(0xd8)]={'https://zenzapi.xyz':''+api};}else global[_0xe15980(0xd8)]={'https://zenzapi.xyz':'process.env.ZENS_API'};global[_0xe15980(0xf6)]=['447405935355',_0xe15980(0xfc)],global[_0xe15980(0xd0)]=[_0xe15980(0xd4),'process.env.PREMIUM'],global[_0xe15980(0xf0)]=process['env'][_0xe15980(0xe7)],global[_0xe15980(0x100)]=process[_0xe15980(0xe2)]['S_AUTHOR'],global[_0xe15980(0xe1)]=_0xe15980(0xd6),global[_0xe15980(0xe6)]=['','!','.','🐦','🐤','🗿'],global['sp']='𒆜',global[_0xe15980(0xf2)]={'success':'ᴀʟʀɪɢʜᴛ\x20ᴅᴏɴᴇ','admin':_0xe15980(0xcc),'botAdmin':_0xe15980(0x102),'owner':_0xe15980(0xe3),'group':_0xe15980(0xe5),'private':_0xe15980(0xdb),'bot':'ꜰᴇᴀᴛᴜʀᴇ\x20ᴏɴʟʏ\x20ꜰᴏʀ\x20ʙᴏᴛ\x20ɴᴜᴍʙᴇʀ\x20ᴜꜱᴇʀ','wait':'ʟᴏᴀᴅɪɴɢ...','endLimit':_0xe15980(0xde)},global[_0xe15980(0xd1)]={'premium':_0xe15980(0xdc),'free':0x64},global[_0xe15980(0xe4)]=fs['readFileSync'](_0xe15980(0xd7)),global[_0xe15980(0xfb)]=fs[_0xe15980(0xfe)](_0xe15980(0xe9));let file=require[_0xe15980(0xf5)](__filename);function _0x10dd(_0x4dcf5c,_0x14f4cd){const _0x503aaa=_0x503a();return _0x10dd=function(_0x10dd99,_0x3aaa78){_0x10dd99=_0x10dd99-0xcc;let _0x3a3e99=_0x503aaa[_0x10dd99];return _0x3a3e99;},_0x10dd(_0x4dcf5c,_0x14f4cd);}fs[_0xe15980(0xee)](file,()=>{const _0x10b439=_0xe15980;fs[_0x10b439(0xea)](file),console[_0x10b439(0xd3)](chalk[_0x10b439(0xda)](_0x10b439(0xed)+__filename+'\x27')),delete require[_0x10b439(0x103)][file],require(file);});
